359d53a30160e21b694d77ddc55322ca2b388174
Scott Edwards <scotteza@gmail.com>
2018-08-07 14:43:10 +0100

    Initial project setup

--- csharp/ApprovalTest.cs
+++ csharp/ApprovalTest.cs
@@ -14,12 +14,12 @@ namespace csharp
         [Test]
         public void ThirtyDays()
         {
-            StringBuilder fakeoutput = new StringBuilder();
+            var fakeoutput = new StringBuilder();
             Console.SetOut(new StringWriter(fakeoutput));
             Console.SetIn(new StringReader("a\n"));
 
             Program.Main(new string[] { });
-            String output = fakeoutput.ToString();
+            var output = fakeoutput.ToString();
             Approvals.Verify(output);
         }
     }
