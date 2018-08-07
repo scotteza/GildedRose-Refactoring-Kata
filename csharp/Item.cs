1ab32b4d6f4e75f0bb45e57cbeb61c7f6fdb721d
Scott Edwards <scotteza@gmail.com>
2018-08-07 15:17:24 +0100

    Added ItemFactory and added 2 constructors to Item class.

--- csharp/Item.cs
+++ csharp/Item.cs
@@ -2,6 +2,17 @@
 {
     public class Item
     {
+        public Item()
+        {
+        }
+
+        public Item(string name, int sellIn, int quality)
+        {
+            Name = name;
+            SellIn = sellIn;
+            Quality = quality;
+        }
+
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
---
+++ csharp/ItemFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace GildedRose
+{
+    public static class ItemFactory
+    {
+        public static Item GetItem(string name, int sellIn, int quality)
+        {
+            return new Item(name, sellIn, quality);
+        }
+    }
+}
