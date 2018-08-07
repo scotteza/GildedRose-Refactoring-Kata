f6c18636355833b7e7bea0bc4b045f43510195be
Scott Edwards <scotteza@gmail.com>
2018-08-07 15:57:09 +0100

    Added tests to check we are creating the correct item types.

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -18,5 +18,29 @@ namespace GildedRose
             app.UpdateQuality();
             Assert.AreEqual("foo", items[0].Name);
         }
+
+        [Test]
+        public void Create_Legendary_Items()
+        {
+            var item = Item.GetItem("Sulfuras, Hand of Ragnaros", 0, 0);
+
+            Assert.That(item, Is.TypeOf<LegendaryItem>());
+        }
+
+        [Test]
+        public void Create_Cheese_Items()
+        {
+            var item = Item.GetItem("Aged Brie", 0, 0);
+
+            Assert.That(item, Is.TypeOf<CheeseItem>());
+        }
+
+        [Test]
+        public void Create_BackstagePass_Items()
+        {
+            var item = Item.GetItem("Backstage passes to a TAFKAL80ETC concert", 0, 0);
+
+            Assert.That(item, Is.TypeOf<BackstagePassItem>());
+        }
     }
 }
