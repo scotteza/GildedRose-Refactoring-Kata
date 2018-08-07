735a8bb86e4f98c7cb10548c7951ee341576ada1
Scott Edwards <scotteza@gmail.com>
2018-08-07 15:30:16 +0100

    Newing up objects now uses static factory method.

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -10,16 +10,16 @@ namespace GildedRose
             Console.WriteLine("OMGHAI!");
 
             IList<Item> items = new List<Item>{
-                new Item ("+5 Dexterity Vest", 10, 20),
-                new Item ("Aged Brie", 2, 0),
-                new Item ("Elixir of the Mongoose", 5, 7),
-                new Item ("Sulfuras, Hand of Ragnaros", 0, 80),
-                new Item ("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert",15,20),
-                new Item("Backstage passes to a TAFKAL80ETC concert",10,49),
-                new Item("Backstage passes to a TAFKAL80ETC concert",5,49),
+                Item.GetItem ("+5 Dexterity Vest", 10, 20),
+                Item.GetItem ("Aged Brie", 2, 0),
+                Item.GetItem ("Elixir of the Mongoose", 5, 7),
+                Item.GetItem ("Sulfuras, Hand of Ragnaros", 0, 80),
+                Item.GetItem ("Sulfuras, Hand of Ragnaros", -1, 80),
+                Item.GetItem("Backstage passes to a TAFKAL80ETC concert",15,20),
+                Item.GetItem("Backstage passes to a TAFKAL80ETC concert",10,49),
+                Item.GetItem("Backstage passes to a TAFKAL80ETC concert",5,49),
 				// this conjured item does not work properly yet
-				new Item ("Conjured Mana Cake", 3, 6)
+				Item.GetItem ("Conjured Mana Cake", 3, 6)
             };
 
             var app = new GildedRose(items);
